eb0254f7357d213f262a4365aaa03756f0111271
CrandoLee <crandolee@gmail.com>
2015-01-24 13:11:00 +0800

    Update1
    Now it can do some easy arithmetic operation.

--- src/com/example/calculator/MainActivity.java
+++ src/com/example/calculator/MainActivity.java
@@ -34,15 +34,15 @@ public class MainActivity extends Activity implements OnClickListener{
 	
 	double x1 = 0,x2 = 0;//定义两个变量
 	
-	String first = ""; //存储第一个要进行运算的字符
-	String second = "";//存储第二个要进行运算的字符
+	double first = 0; //存储第一个要进行运算的数字
+	double second = 0;//存储第二个要进行运算的数字
 	String expression = "";//存储输入的式子
-	
+	String expression_backup = "";//存储输入式子的备份
+	String last_input = "";//存储上一个输入的字符
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("hahahhaa");
 		btn_0 = (Button) findViewById(R.id.btn_0);
 		btn_1 = (Button) findViewById(R.id.btn_1);
 		btn_2 = (Button) findViewById(R.id.btn_2);
@@ -103,8 +103,55 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.btn_7:
 		case R.id.btn_8:
 		case R.id.btn_9:
+			//如果是上述数字，直接在式子末尾添加
 			expression = expression + ((Button) v).getText();
+			//记录上一次输入的字符
+			last_input = (String) ((Button) v).getText();
 			et_input.setText(expression);
+			break;
+		case R.id.btn_add:
+		case R.id.btn_minus:
+		case R.id.btn_multiple:
+		case R.id.btn_divide:
+			//输入加减乘除，如果上一个输入的是加减乘除符号，这次点击无效
+			if (last_input.equals("+") || last_input.equals("-") || last_input.equals("×") || last_input.equals("÷")) {
+				
+			}
+			else {
+				expression = expression + ((Button) v).getText();
+				last_input = (String) ((Button) v).getText();
+				et_input.setText(expression);
+			}
+			break;
+		case R.id.btn_del:
+			//退格键，如果式子长度不为零，删除一个字符,否则不执行删除操作
+			if(expression.length() > 0){
+				expression = expression.substring(0,expression.length() - 1);
+				et_input.setText(expression);
+			}
+
+			break;
+		case R.id.btn_clear:
+			//删除整个式子，并且把所有标志归为初始状态
+			expression = "";
+			last_input = "";
+			first = 0;
+			second = 0;
+			et_input.setText(expression);
+			break;
+		case R.id.btn_point:
+			//输入点的内容,如果之前输入小数点，则不允许连续输入两个小数点
+			if (last_input.equals(".")) {
+				
+			}
+			else {
+				expression = expression + ((Button) v).getText();
+				last_input = (String) ((Button) v).getText();
+				et_input.setText(expression);
+			}
+			break;	
+		case R.id.btn_equal:
+			getResult();
 			break;
 		default:
 			break;
@@ -113,6 +160,194 @@ public class MainActivity extends Activity implements OnClickListener{
 
 	
 	public void getResult(){
+		//计算式子结果2+2x3
+		expression_backup = expression;
+		//一直做乘除
+		while(doMultiplyAndDivide() != -1);
+		//一直做加减
+		while(doAddAndMinus() != -1);
 		
+		et_input.setText(expression);
+		
+		
+	}
+	
+	//计算乘除法
+	public int doMultiplyAndDivide(){
+		//计算乘除
+		int position1 = 0;
+		int position2 = 0;
+		double temp = 0;
+		int multilocation = expression.indexOf("×");
+		int dividelocation = expression.indexOf("÷");
+		String frontString = "";
+		String backString = "";
+		if (multilocation != -1 || dividelocation != -1){
+			if (multilocation > dividelocation){
+				//做乘法
+				position1 = getFirst(multilocation);
+				if (position1 == multilocation) {
+					position1 = -1;
+				}
+				position2 = getSecond(multilocation);
+				if(position2 == -1 || position2 == multilocation){
+					position2 = expression.length();
+				}
+				//取得两个数字
+				String s1 = expression.substring(position1 + 1,multilocation);
+				String s2 = expression.substring(multilocation + 1,position2);
+				first = Double.parseDouble(s1);
+				second = Double.parseDouble(s2);
+				temp  = first * second;
+				if(position1 == -1){
+					frontString = "";
+				}
+				else{
+					frontString = expression.substring(0,position1);
+				}
+				if (position2 == expression.length()) {
+					backString = "";
+				}
+				else{
+					backString = expression.substring(position2 - 1,expression.length());
+				}
+				expression = frontString + temp + backString;
+			}
+			else{
+				//做除法
+				position1 = getFirst(dividelocation);
+				if (position1 == dividelocation) {
+					position1 = -1;
+				}
+				position2 = getSecond(dividelocation);
+				if(position2 == -1 || position2 == dividelocation){
+					position2 = expression.length();
+				}
+				//取得两个数字
+				String s1 = expression.substring(position1 + 1,dividelocation);
+				String s2 = expression.substring(dividelocation + 1,position2);
+				first = Double.parseDouble(s1);
+				second = Double.parseDouble(s2);
+				temp  = first / second;
+				if(position1 == -1){
+					frontString = "";
+				}
+				else{
+					frontString = expression.substring(0,position1);
+				}
+				if (position2 == expression.length()) {
+					backString = "";
+				}
+				else{
+					backString = expression.substring(position2 - 1,expression.length());
+				}
+				expression = frontString + temp + backString;
+			}
+		}
+		else{
+			return -1;
+		}
+		return 0;
+	}
+	
+	//计算加减法
+	public int doAddAndMinus(){
+		//计算加减
+		int position1 = 0;
+		int position2 = 0;
+		double temp = 0;
+		int addlocation = expression.indexOf("+");
+		int minuslocation = expression.indexOf("-");
+		String frontString = "";
+		String backString = "";
+		if (addlocation != -1 || minuslocation != -1){
+			if (addlocation > minuslocation){
+				//做加法
+				position1 = getFirst(addlocation);
+				if (position1 == addlocation) {
+					position1 = -1;
+				}
+				position2 = getSecond(addlocation);
+				if(position2 == -1 || position2 == addlocation){
+					position2 = expression.length();
+				}
+				Log.i("Tag", "position1: " + position1);
+				Log.i("Tag", "position2: " + position2);
+				//取得两个数字
+				String s1 = expression.substring(position1 + 1,addlocation);
+				
+				String s2 = expression.substring(addlocation + 1,position2);
+				first = Double.parseDouble(s1);
+				second = Double.parseDouble(s2);
+				temp  = first + second;
+				Log.i("Tag", "s1: " + s1);
+				Log.i("Tag", "s2: " + s2);
+				Log.i("Tag", "temp: " + temp);
+				if(position1 == -1){
+					frontString = "";
+				}
+				else{
+					frontString = expression.substring(0,position1);
+				}
+				if (position2 == expression.length()) {
+					backString = "";
+				}
+				else{
+					backString = expression.substring(position2 - 1,expression.length());
+				}
+				expression = frontString + temp + backString;
+				Log.i("Tag", "expression: " + expression);
+			}
+			else{
+				//做减法
+				position1 = getFirst(minuslocation);
+				if (position1 == addlocation) {
+					position1 = -1;
+				}
+				position2 = getSecond(minuslocation);
+				if(position2 == -1 || position2 == minuslocation){
+					position2 = expression.length();
+				}
+				//取得两个数字
+				String s1 = expression.substring(position1 + 1,minuslocation);
+				String s2 = expression.substring(minuslocation + 1,position2);
+				first = Double.parseDouble(s1);
+				second = Double.parseDouble(s2);
+				temp  = first - second;
+				if(position1 == -1){
+					frontString = "";
+				}
+				else{
+					frontString = expression.substring(0,position1);
+				}
+				if (position2 == expression.length()) {
+					backString = "";
+				}
+				else{
+					backString = expression.substring(position2 - 1,expression.length());
+				}
+				expression = frontString + temp + backString;
+			}
+		}
+		else{
+			return -1;
+		}
+		return 0;
+	}
+	
+	//取得运算符之前的运算符位置
+	public int getFirst(int position)
+	{
+		int position1 = expression.indexOf("+",0);
+		int position2 = expression.indexOf("-",0);
+		return position1 > position2? position1: position2;
+	}
+	
+	//取得运算符之后的运算符位置
+	public int getSecond(int position)
+	{
+		int position1 = expression.indexOf("+",position);
+		int position2 = expression.indexOf("-",position);
+		return position1 > position2? position1: position2;
 	}
 }
